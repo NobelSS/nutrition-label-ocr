dab85f003b92aaafdf68c62d4198e1517cbd1460
NobelSS <nobelshansetiono@gmail.com>
2025-05-27 22:13:56 +0700

    feat: Object Detection with Faster R-CNN

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 from object_detection import detect_object
 from deskew import deskew
 from preprocess import preprocess
 from ocr import use_tesseract
 
-image_path = "./dataset/real/20250510_181112.jpg"
+image_path = "dataset/real/20250510_185639.jpg"
 
 cropped = detect_object(image_path, show_result=False)
 deskewed = deskew(cropped, show_result=False)
 output = preprocess(deskewed, debug=True)
 ocr_text = use_tesseract(output)
 
-print(f'\nDetected Text: \n {ocr_text}')
+print(f'/nDetected Text: \n {ocr_text}')
--- object_detection.py
+++ object_detection.py
@@ -1,55 +1,59 @@
-import os
-from inference_sdk import InferenceHTTPClient
-from PIL import Image, ImageOps
-import matplotlib.pyplot as plt
-from dotenv import load_dotenv
-import numpy as np
 import cv2
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision.transforms as T
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-load_dotenv()
+def get_model(num_classes: int = 2, model_path: str = "./model/best_model.pth"):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-API_KEY = os.getenv("ROBOFLOW_API_KEY")
-if not API_KEY:
-    raise ValueError("Missing ROBOFLOW_API_KEY in environment variables.")
+    model = fasterrcnn_resnet50_fpn(weights=None)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-CLIENT = InferenceHTTPClient(
-    api_url="https://serverless.roboflow.com",
-    api_key=API_KEY
-)
+    state_dict = torch.load(model_path, map_location=device, weights_only=True)
+    model.load_state_dict(state_dict)
+    model.to(device)
+    model.eval()
 
+    return device, model
 
-def detect_object(image_path: str, show_result: bool = True, model_id: str = 'nutrition-label-table/3'):
-    image = Image.open(image_path)
+def detect_object(image_path: str, show_result: bool = True):
 
-    result = CLIENT.infer(image_path, model_id=model_id)
+    device, model = get_model(num_classes=2, model_path="./model/best_model.pth")
 
-    if not result["predictions"]:
-        print("No predictions found.")
-        return None
+    image = cv2.imread(image_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized_image = cv2.resize(gray_image, (640, 640))
+    rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_GRAY2RGB)
 
-    top_pred = max(result["predictions"], key=lambda x: x["confidence"])
+    pil_image = Image.fromarray(rgb_image)
+    img_tensor = T.ToTensor()(pil_image).unsqueeze(0).to(device)
 
-    x = top_pred["y"]
-    y = top_pred["x"]
-    w = top_pred["width"]
-    h = top_pred["height"]
+    with torch.no_grad():
+        prediction = model(img_tensor)[0]
 
-    left = int(x  - w / 2)
-    top = int(y  - h / 2)
-    right = int(x + w / 2)
-    bottom = int(y + h / 2)
+    boxes = prediction["boxes"]
+    scores = prediction["scores"]
 
-    cropped = image.crop((left, top, right, bottom))
-    cropped_display = ImageOps.exif_transpose(cropped) # Fix orientation for image with EXIF tag
+    cropped_result = None
+    for box, score in zip(boxes, scores):
+        if score < 0.5:
+            continue
+        xmin, ymin, xmax, ymax = box.int().cpu().numpy()
+        cropped_result = resized_image[ymin:ymax, xmin:xmax]
 
-    if show_result:
-        plt.figure()
-        plt.imshow(cropped_display)
-        plt.axis("off")
-        plt.show()
+        if show_result is True and cropped_result is not None:
+            plt.imshow(cropped_result, cmap="gray")
+            plt.title(f"Confidence: {score:.2f}")
+            plt.axis("off")
+            plt.show()
+
+        break
+
+    if cropped_result is not None:
+        return cv2.cvtColor(cropped_result, cv2.COLOR_GRAY2BGR)
     
-    print('Cropping nutrition label...')
-    cropped_np = np.array(cropped_display)
-    cropped_bgr = cv2.cvtColor(cropped_np, cv2.COLOR_RGB2BGR)
-    
-    return cropped_bgr
+    return None
